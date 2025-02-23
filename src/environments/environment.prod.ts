d6a12afba9ed06d3cae1db4719a681851915d239
ramaeon <Rama.eon@gmail.com>
2025-02-23 21:37:26 +0530

    apiBaseUrlV1 changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
-    apiBaseUrlV1: '18.60.44.176:8000/v1/auth'
-  };  
+  production: true,
+  apiBaseUrlV1: 'http://18.60.44.176:8000/v1/auth'
+};
