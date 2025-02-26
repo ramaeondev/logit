3e1ba3f101fa0af77183e60fb355591b9cb05d60
ramaeon <Rama.eon@gmail.com>
2025-02-26 11:16:13 +0530

    fix: routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,31 +5,37 @@ export const routes: Routes = [
         path: 'login',
         loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
     },
-    {   path: 'registration',
+    {
+        path: 'registration',
         loadComponent: () => import('./registration/registration.component').then(m => m.RegistrationComponent)
     },
-    {   path: 'forgot-password',
+    {
+        path: 'forgot-password',
         loadComponent: () => import('./forget-password/forget-password.component').then(m => m.ForgetPasswordComponent)
     },
-    {   path: 'reset-password',
+    {
+        path: 'reset-password',
         loadComponent: () => import('./reset-password/reset-password.component').then(m => m.ResetPasswordComponent)
     },
-    {   path: 'dashboard',
+    {
+        path: 'dashboard',
         loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
     },
-    {   path: 'privacy-statement',
+    {
+        path: 'privacy-statement',
         loadComponent: () => import('./privacy-statement/privacy-statement.component').then(m => m.PrivacyStatementComponent)
     },
-    {   path: 'end-user-agreement',
+    {
+        path: 'end-user-agreement',
         loadComponent: () => import('./end-user-agreement/end-user-agreement.component').then(m => m.EndUserAgreementComponent)
     },
-    { 
+    {
         path: '404',
         loadComponent: () => import('./pages/not-found/not-found.component').then(m => m.NotFoundComponent)
-      },
-      {
+    },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    {
         path: '**',
         redirectTo: '/404'
-      },
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
+    }
 ];
