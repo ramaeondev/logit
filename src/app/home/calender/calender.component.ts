9490511bcf5038c9a00078bef4770cacd5f4131d
ramaeon <Rama.eon@gmail.com>
2025-02-26 15:28:36 +0530

    Feat: Initial Custom Calender is Ready

--- src/app/home/calender/calender.component.ts
+++ src/app/home/calender/calender.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { startOfMonth, endOfMonth, eachDayOfInterval, format, isToday } from 'date-fns';
 
 
 @Component({
@@ -14,10 +15,43 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 })
 export class CalenderComponent {
 
-  selectedDate:any;
+  currentMonth: string = '';
+  daysInMonth: { date: Date; eventTypes: { type: string; count: number }[] }[] = [];
+  
   @Output() dateSelected = new EventEmitter<Date>();
-  onDateSelect(date: Date) {
-    this.dateSelected.emit(date);
+
+  // Sample event data (Replace with actual data from API)
+  eventData: { [key: string]: { type: string; count: number }[] } = {
+    '2025-02-01': [{ type: 'work', count: 2 }, { type: 'personal', count: 1 }],
+    '2025-02-10': [{ type: 'meeting', count: 3 }],
+    '2025-02-15': [{ type: 'personal', count: 5 }],
+    '2025-02-26': [{ type: 'work', count: 4 }, { type: 'meeting', count: 2 }]
+  };
+
+  ngOnInit() {
+    this.generateMonthView(new Date());
+  }
+
+  generateMonthView(date: Date) {
+    this.currentMonth = format(date, 'MMMM yyyy');
+    
+    const days = eachDayOfInterval({ 
+      start: startOfMonth(date), 
+      end: endOfMonth(date) 
+    });
+
+    this.daysInMonth = days.map(day => ({
+      date: day,
+      eventTypes: this.eventData[format(day, 'yyyy-MM-dd')] || []
+    }));
+  }
+
+  selectDate(day: Date) {
+    this.dateSelected.emit(day);
+  }
+
+  isToday(date: Date): boolean {
+    return isToday(date);
   }
 
 }
