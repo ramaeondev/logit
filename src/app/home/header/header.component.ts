d1c37a8b6197dd07813a051e69731fb1e23190b1
ramaeon <Rama.eon@gmail.com>
2025-03-14 15:49:17 +0530

    feat : UI Improvements
    1.  Logout button added
    2. Cloudnotes Text logo added

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,10 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal, WritableSignal } from '@angular/core';
 import { LogoComponent } from '../../logo/logo.component';
+import { environment } from '../../../environments/environment';
+import { User, ChevronLeft, LucideAngularModule } from 'lucide-angular'; // Import the User icon
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, LogoComponent],
+  imports: [CommonModule, LogoComponent, LucideAngularModule, MatButtonModule, MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   standalone: true,
@@ -12,4 +17,14 @@ import { LogoComponent } from '../../logo/logo.component';
 })
 export class HeaderComponent {
 
+  appName: WritableSignal<string> = signal<string>('');
+  User = User;
+  ChevronLeft = ChevronLeft
+  constructor(private readonly authService: AuthService) { 
+        this.appName.set(environment.appName);
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
